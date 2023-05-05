2766c8f8761796163c2540dfd91f57ad10d003de
Obizzi <kramzyvine@icloud.com>
2023-05-05 13:43:20 +0300

    The "Map and Lambda function" task was missing functionality for 1 and 0 as inputs

--- Map and Lambda functions.py
+++ Map and Lambda functions.py
@@ -5,10 +5,14 @@ cube = lambda x: pow(x, 3) # complete the lambda function
 
 def fibonacci(n):
     fib = [0, 1]
-    for i in range(2, n):
-        fib.append(sum(fib[i-2:i]))
+    if n == 0:
+        fib.clear()
+    elif n == 1:
+        fib = [0, ]
+    else:
+        for i in range(2, n):
+            fib.append(sum(fib[i - 2:i]))
     return fib
-
 if __name__ == '__main__':
     n = int(input())
     print(list(map(cube, fibonacci(n))))
