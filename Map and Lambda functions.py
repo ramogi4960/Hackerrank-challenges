5bb200f85e696c281182571453da06b89e0f423e
Obizzi <kramzyvine@icloud.com>
2023-05-05 13:35:40 +0300

    Added Map and Lambda function

---
+++ Map and Lambda functions.py
@@ -0,0 +1,14 @@
+# in this challenge, we first take in an int N, representing the first N fibonacci numbers
+# we are supposed to print out the cube of each number in the fibonacci list
+cube = lambda x: pow(x, 3) # complete the lambda function
+
+
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(sum(fib[i-2:i]))
+    return fib
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
