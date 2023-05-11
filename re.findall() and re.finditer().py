21935d85717ee2d07e0f2499fd10e7b1a39136dc
Obizzi <kramzyvine@icloud.com>
2023-05-11 20:49:16 +0300

    modified findall() and finditer() to facilitate cases where there is no regex match

--- re.findall() and re.finditer().py
+++ re.findall() and re.finditer().py
@@ -1,35 +1,29 @@
-# our task here is to find substrings of a string S that contain 2 or more vowels
-# these substrings must lie in between 2 consonants and should only contain vowels
-# we can do this by checking if a consonant is followed by consecutive vowels
-# we can use regex for this by specifying the pattern in the regex pattern
+# in this task, we are first given a string
+# we are supposed to print all vowels occurring consecutively in between consonants
+# if there are no consecutively occurring vowels, we print -1
+# we can do this by first defining a re pattern to expose the consecutive vowels
+# we can append them into a list then print them out vertically
 # concsonants - QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm
 # vowels - AEIOUaeiou
-# I waaant you too beet
-# abaaaboopii
-# there is no match here
 import re
-p, matches = r"[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm][AEIOUaeiou]{2,}", []
-word = input().split()
-if len(word) > 1:
-    for item in word:  # this will be a list of matches with a consonant at the start
-        if item[-1] in "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm":
-            matches.append(re.findall(p, item))
+s = input().split()
+matches, consonants, vowels = [], "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm", "AEIOUaeiou"
+pattern = re.compile(r"[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm][AEIOUaeiou]{2,}")
+for i in s:
+    if i[-1] in consonants and pattern.search(i) is not None:
+        f = pattern.findall(i)
+        for l in f:
+            matches.append(l)
+    elif i[-1] in vowels and pattern.search(i) is not None:
+        f = pattern.findall(i)
+        for l in f:
+            matches.append(l)
+        matches.pop()
+
+
+if matches:
+    for i in matches:
+        print(i[1:])
 else:
-    for r in word:
-        if r[-1] in "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm":
-            matches.append(re.findall(p, r))
-        else:
-            matches.append(re.findall(p, r))
-            matches[-1].pop()
-
-
-# for i in matches:
-#     if len(i) == 0:
-#         matches.remove(i)
-
-print(matches)
-
-# if len(matches) <= 1:
-#     print("-1")
-# else:
-#     print(i for i in matches)
+    print(-1)
+    
--- tests.py
+++ tests.py
@@ -1,22 +1,62 @@
+# import re
+# word = """
+# I went to the zoo, I saw a fat monkey and I thought it was you
+# You should have seen it
+# I'm a youth man
+# """
+# b = "aaabaab"
+# c = "aa"
+# pat = re.compile(c)
+# list1 = []
+# for i in range(len(b)):
+#     y = b[i:]
+#     if pat.match(y) is None:
+#         pass
+#     else:
+#         l = pat.match(y)
+#         list1.append([i, (i+len(c))-1])
+#
+#
+# print(list1)
+
+""" ..................................................................................................."""
+
+# our task here is to find substrings of a string S that contain 2 or more vowels
+# these substrings must lie in between 2 consonants and should only contain vowels
+# we can do this by checking if a consonant is followed by consecutive vowels
+# we can use regex for this by specifying the pattern in the regex pattern
+# concsonants - QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm
+# vowels - AEIOUaeiou
+# I waaant you too beet
+# abaaaboopii
+# there is no match here
+
+
+# import re
+# p, matches = r"[QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm][AEIOUaeiou]{2,}", []
+# word = input().split()
+# if len(word) > 1:
+#     for item in word:  # this will be a list of matches with a consonant at the start
+#         if item[-1] in "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm":
+#             matches.append(re.findall(p, item))
+# else:
+#     for r in word:
+#         if r[-1] in "QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm":
+#             matches.append(re.findall(p, r))
+#         else:
+#             matches.append(re.findall(p, r))
+#             matches[-1].pop()
+#
+#
+# # for i in matches:
+# #     if len(i) == 0:
+# #         matches.remove(i)
+#
+# print(matches)
+#
+# # if len(matches) <= 1:
+# #     print("-1")
+# # else:
+# #     print(i for i in matches)
 import re
-word = """
-I went to the zoo, I saw a fat monkey and I thought it was you
-You should have seen it
-I'm a youth man
-"""
-b = "aaabaab"
-c = "aa"
-pat = re.compile(c)
-list1 = []
-for i in range(len(b)):
-    y = b[i:]
-    if pat.match(y) is None:
-        pass
-    else:
-        l = pat.match(y)
-        list1.append([i, (i+len(c))-1])
-
-
-print(list1)
-
-
+print(bool(re.findall(r"aa", "Kevin")))
