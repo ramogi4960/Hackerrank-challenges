93a207f02ab1445057adf9150dd4a2e2306c8b86
Obizzi <kramzyvine@icloud.com>
2023-05-12 12:17:53 +0300

    Added validating roman numbers

---
+++ Validating rman numerals.py
@@ -0,0 +1,11 @@
+# in this task, we take in an input as a string then determine if it's a roman number or not
+# we print True if it is else False
+# to validate if the string is a roman number, we need to first know how to represent roman numbers
+# a roman number within range(1, 4000) can either start with either I, V, X, L, C or D
+# all numbers in range(1, 10) have to start with I, V
+# all numbers in range(10, 50) have to start with X
+# all numbers in range(50, 90) have to start with L
+# all numbers in range(90, 100) have to start with XC
+
+import re
+f = re.compile()
--- tests.py
+++ tests.py
@@ -1,2 +1,3 @@
-x = complex(3, 4)
-print(abs(x))
+import re
+x = re.compile(r"M{,3}(D?C{,3}|C[DM])?(L?X{,3}|X[LC])?(V?I{,3}|I[VX])?$")
+print(bool(x.match(input("Enter string to check if it's a valid roman number: "))))
