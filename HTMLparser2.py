1059cdacf46d9beaf78d0b75fad8a8215284954f
Obizzi <kramzyvine@icloud.com>
2023-05-15 16:46:31 +0300

    Edited HTMLparser2.py to  be flexible when handling multiline comments and data as \n

--- HTMLparser2.py
+++ HTMLparser2.py
@@ -1,53 +1,28 @@
-# in this task, we are given html code and have to print out comments and data in a particular way
-# multi-line and single-line comments should be printed in a different manner
-# we need to find out if the comment is a multi-line or not
-# we can join lines of the html code with \n for items that fulfill defined conditions for a multi-line
-# if not, we join the lines with ""
-
-# """
-# 4
-# <!--This is a
-# Multiline comment
-# <div> my data is commented </div>
-# end of comment-->
-# """
-# """
-# 3
-# <!--Kam se kam 6
-# test case to bante hain
-# re bhai-->
-# """
-
 from html.parser import HTMLParser
+import re
 
 
-class Remy(HTMLParser):
+class MyHTMLParser(HTMLParser):
+
     def handle_comment(self, data):
-        if "\n" in data:
-            print(f">>> Multi-line Comment\n{data}")
+        if re.search(r'\n', data):
+            print('>>> Multi-line Comment')
+            print(data)
         else:
-            print(f">>> Single-line Comment\n{data}")
+            print('>>> Single-line Comment')
+            print(data)
 
     def handle_data(self, data):
-        print(f">>> Data\n{data}")
+        if data != '\n':
+            print(">>> Data")
+            print(data)
 
 
-html_code = [input() for _ in range(int(input()))]
-new_html_code = []
-for i in range(len(html_code)):  # section to handle multi-line comments in a different way
-    if html_code[i][:4] == "<!--" and html_code[i][-3:] != "-->":
-        feeder = [html_code[i]]
-        for item in html_code[i + 1:]:
-            feeder.append(item)
-            if item[-3:] == "-->":
-                break
-        new_html_code.append("\n".join(feeder))
-    elif html_code[i][:4] == "<!--" and html_code[i][-3:] == "-->":
-        new_html_code.append(html_code[i])
-    elif html_code[i][:4] != "<!--" and html_code[i][-3:] == "-->":
-        pass
-    else:
-        new_html_code.append(html_code[i])
+html = ""
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
 
-my_object = Remy()
-my_object.feed("".join(new_html_code))
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
--- tests.py
+++ tests.py
@@ -1,17 +1,32 @@
-a = set(""">>> Multi-line Comment
-[if IE 9]>IE9-specific content
-<![endif]
->>> Data
- Welcome to HackerRank
->>> Single-line Comment
-[if IE 9]>IE9-specific content<![endif]""")
-
-b = set(""">>> Multi-line comment
-[if IE 9]>IE9-specific content
-<![endif]
->>> Data
- Welcome to HackerRank
->>> Single-line comment
-[if IE 9]>IE9-specific content<![endif]""")
-
-print(a.difference(b))
+# # using re to find consecutives
+# # ??????
+# # let me think
+#
+# from html.parser import HTMLParser
+# import re
+#
+# html_code, new_html_code = [input() for _ in range(int(input()))], []
+# for number in range(len(html_code)):
+#     if re.search(r"^<!--.*", html_code[number]):
+#         new_html_code.append(html_code[number] + "\n")
+#     elif re.search(r".*-->$", html_code[number]):
+#         new_html_code.append(html_code[number])
+#     elif re.search(r"\\n$", html_code[number - 1]) and number > 0:
+#         new_html_code.append(html_code[number] + "\n")
+#     else:
+#         new_html_code.append(html_code[number])
+#
+#
+# class My_Parser(HTMLParser):
+#     def handle_comment(self, data):
+#         if "\n" in data:
+#             print(f">>> Multi-line Comment\n{data}")
+#         else:
+#             print(f">>> Single-line Comment\n{data}")
+#
+#     def handle_data(self, data):
+#         print(f">>> Data\n{data}")
+#
+#
+# my_object = My_Parser()
+# my_object.feed("".join(new_html_code))
