7af388bbfc4ed8a66a1fbce3fad1f2f87cf8f8e3
Obizzi <kramzyvine@icloud.com>
2023-05-24 13:13:19 +0300

    added validating Postal codes. Should do thorough revision on regular expressions

--- Validating Postal Codes.py
+++ Validating Postal Codes.py
@@ -1,12 +1,10 @@
 # the postal code should be within range(100000, 1000000)
 # the postal code can only have one alternating repetitive number
+
 import re
-
-regex_integer_in_range = r"[0-9]{6}"	# Do not delete 'r'.
-regex_alternating_repetitive_digit_pair = r"(\d){1}"	# Do not delete 'r'.
-
-
+regex_integer_in_range = r"^[0-9]{6}$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=.\1{1})"
 
 P = input()
 
-print (bool(re.match(regex_integer_in_range, P)) and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
+print(bool(re.match(regex_integer_in_range, P)) and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
--- tests.py
+++ tests.py
@@ -1,3 +1,3 @@
 import re
-alt = r"([0-9]{1})[0-9]{1}\1"
+alt = r"(\d)(?=.\1{1})"
 print(re.findall(alt, input()))
