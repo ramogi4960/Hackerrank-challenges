f0b07363cf278729ce30fc3a2031df296df6f6b4
Obizzi <kramzyvine@icloud.com>
2023-05-24 10:44:42 +0300

    finished Decorators 2 Name Directory.py and added test cases with their solutions

--- Decorators 2 Name Directory.py
+++ Decorators 2 Name Directory.py
@@ -8,13 +8,56 @@ Test case
 Mike Thomson 20 M
 Robert Bustle 32 M
 Andria Bustle 30 F
+
+6
+Blossom Michael 9 F
+Bubbles Kevin 666666666666666 F
+Buttercup Jake 4444444444444444444444444444444444444444444444444444444444444444 F
+Michael Blossom 8888 M
+Kevin Bubbles 7777777 M
+Jake Buttercup 555555555555555555555555555555 M
+
+Ms. Blossom Michael
+Mr. Michael Blossom
+Mr. Kevin Bubbles
+Ms. Bubbles Kevin
+Mr. Jake Buttercup
+Ms. Buttercup Jake
+
+10
+Blossom Blossom 10 F
+Bubbles Blossom 10 F
+Buttercup Blossom 9 F
+Blossom Bubbles 10 F
+Bubbles Bubbles 10 F
+Buttercup Bubbles 200 F
+Blossom Buttercup 30 F
+Bubbles Buttercup 30 F
+Buttercup Buttercup 10 F
+Butterblossom Bubblescup 10 F
+
+Ms. Buttercup Blossom
+Ms. Blossom Blossom
+Ms. Bubbles Blossom
+Ms. Blossom Bubbles
+Ms. Bubbles Bubbles
+Ms. Buttercup Buttercup
+Ms. Butterblossom Bubblescup
+Ms. Blossom Buttercup
+Ms. Bubbles Buttercup
+Ms. Buttercup Bubbles
 """
 
 
 def person_lister(f):
-    def inner(people):
-        sorted_list = sorted(people, key=itemgetter(2))
 
+    def inner(people):
+        a = []
+        for i in people:
+            i[2] = int(i[2])
+        sorted_list = sorted(people, key=itemgetter(2))
+        value = list(map(lambda x: f(x), sorted_list))
+        return value
     return inner
 
 
