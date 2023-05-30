160ed600ad329bcbbba3958788fd2c223f207b08
Kevin Obuya <kramzyvine@icloud.com>
2023-05-30 12:06:08 +0300

    added identifying comments.py

---
+++ Regular Expressions/identifying comments.py
@@ -0,0 +1,7 @@
+import re
+import sys
+
+if __name__ == '__main__':
+    codes = sys.stdin.read()
+    for i in re.findall(r'\/\/.*|\/\*.*(?:[^\*\/]\n.*)*(?<=\*\/)',codes):
+        print('\n'.join([j.strip() for j in re.split(r'\n',i)]))
--- tests.py
+++ tests.py
@@ -1,2 +1,7 @@
-import math
-print(math.lcm(10, 20, 30, 15))
+# import re
+# import sys
+#
+# if __name__ == '__main__':
+#     codes = sys.stdin.read()
+#     for i in re.findall(r'\/\/.*|\/\*.*(?:[^\*\/]\n.*)*(?<=\*\/)',codes):
+#         print('\n'.join([j.strip() for j in re.split(r'\n',i)]))
