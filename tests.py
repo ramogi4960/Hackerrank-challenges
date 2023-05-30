98b71c5994daa86b121e2c07d0a35ceb39019667
Kevin Obuya <kramzyvine@icloud.com>
2023-05-30 13:09:29 +0300

    added Detecting valid latitude and longitude pairs.py

--- tests.py
+++ tests.py
@@ -1,7 +1,1 @@
-# import re
-# import sys
-#
-# if __name__ == '__main__':
-#     codes = sys.stdin.read()
-#     for i in re.findall(r'\/\/.*|\/\*.*(?:[^\*\/]\n.*)*(?<=\*\/)',codes):
-#         print('\n'.join([j.strip() for j in re.split(r'\n',i)]))
+print(bool(-90<float(-20)))
