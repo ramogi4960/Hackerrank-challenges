d56a187081a41e5c51631a52854ae74623d0297f
Kevin Obuya <kramzyvine@icloud.com>
2023-06-05 10:46:45 +0300

    finally finished Detecting html attributes and managed to complete all regex problems from hackerrank

--- Regular Expressions/Detecting html attributes.py
+++ Regular Expressions/Detecting html attributes.py
@@ -1,13 +1,42 @@
 import re
-"""
-<li style="-moz-float-edge: content-box">... that <a href="/wiki/Orval_Overall" title="Orval Overall">Orval Overall</a> <i>(pictured)</i> is the only <b><a href="/wiki/List_of_Major_League_Baseball_pitchers_who_have_struck_out_four_batters_in_one_inning" title="List of Major League Baseball pitchers who have struck out four batters in one inning">Major League Baseball player to strike out four batters in one inning</a></b> in the <a href="/wiki/World_Series" title="World Series">World Series</a>?</li>
-"""
-x = input()
-pat = re.compile(r"(?<=<)(\w+)(\s.*?)(?=>)")
-l = list(pat.findall(x))
-for i in l:
-    att, tag = [], i[0]
-    for item in list(re.findall(r"(?<=\s)\w*?(?==)", i[1])):
-        att.append(item)
-    a = ",".join(att)
-    print(f"{tag}:{a}")
+html_lines, l = [input() for _ in range(int(input()))], []
+for item in html_lines:
+    if re.search(r"(?<=<)[^/].*?(?=>)", item):
+        for i in list(re.findall(r"(?<=<)[^/].*?(?=>)", item)):
+            l.append(i)
+
+my_dict = {}
+for item in l:
+    if re.search(r"(?<=\s)\w+(?==)", item):
+        b = re.match(r"\w+", item).group()
+        if b in my_dict.keys():
+            for i in list(re.findall(r"(?<=\s)\w+?(?==)", item)):
+                if i in my_dict[b]:
+                    pass
+                else:
+                    my_dict[b].append(i)
+        else:
+            my_dict[b] = []
+            for i in list(re.findall(r"(?<=\s)\w+?(?==)", item)):
+                my_dict[b].append(i)
+    else:
+        my_dict[item] = []
+
+x = []
+for item in my_dict.keys():
+    a = []
+    if not my_dict[item]:
+        a.append(item)
+    else:
+        a.append(item)
+        a.append(my_dict[item])
+    x.append(a)
+
+
+for item in sorted(x):
+    if len(item) == 1:
+        print(item[0]+":")
+    else:
+        a = item[0]
+        b = ",".join(sorted(item[1]))
+        print(a+":"+b)
