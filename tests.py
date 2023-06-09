fabaa8b1345a66fbded805bf6c9fe4794500374e
Kevin <kramzyvine@gmail.com>
2023-06-09 13:12:30 +0300

    finished linked lists.py

--- tests.py
+++ tests.py
@@ -1,28 +1,13 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.pointer = None
+a = [
+    {"name": "Kevin",
+     "age": 31},
+    {"name": "Alice",
+     "age": 30}
+]
+# result = dict(sub.values() for sub in a)
+# print(result)
+# for i in a:
+#     print(i.values())
 
-
-class LinkedList:
-    def __init__(self):
-        self.item = None
-
-    def printing(self):
-        printval = self.item
-        while printval is not None:
-            print(printval.data)
-            printval = printval.pointer
-
-
-
-a1 = LinkedList()
-a1.item = Node("Monday")
-a2 = Node("Tuesday")
-a3 = Node("Wednesday")
-
-# linking a1 to a2
-a1.item.pointer = a2
-a2.pointer = a3
-
-a1.printing()
+for i in a:
+    print(dict(i.values()))
