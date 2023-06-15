e1a3b7236da640ce7f2c63b435b414cb4cccc57f
Kevin <kramzyvine@icloud.com>
2023-06-15 23:42:10 +0300

    started implementation of a binary search tree using linked lists

---
+++ 30 days of code/Binary search tree.py
@@ -0,0 +1,48 @@
+"""
+test case:
+7
+3
+5
+2
+1
+4
+6
+7
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root is None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+        else:
+            a = max(self.getHeight(root.left), self.getHeight(root.right))
+            return a
+
+
+T = int(input())
+myTree = Solution()
+root = None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+height = myTree.getHeight(root)
+print(height)
